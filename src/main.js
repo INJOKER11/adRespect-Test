5e882ad9a16181c8aba41bc239d6580dbe483785
Pavel <mig20047@gmail.com>
2026-07-26 20:00:48 +0200

    feat: add oferta section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 import "./style.css";
-import "@fontsource/inter";
+
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/500-italic.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/600-italic.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/700-italic.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/800-italic.css";
+
 import "@fontsource/montserrat";
